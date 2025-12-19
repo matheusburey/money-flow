31996dd24caef320b68ee56e83a8e65b4da19265
matheusburey <matheusbureidev@gmail.com>
2025-12-19 11:55:50 -0300

    feat: implement refresh token

--- server/middleware/protected.ts
+++ server/middleware/protected.ts
@@ -1,21 +1,29 @@
 export default defineEventHandler((event) => {
-  // ignore health check and auth routes
-  const url = event.node.req.url!
-  if (!url.startsWith('/api') || ['/api/health', '/api/auth/login', '/api/auth/signup'].includes(url)) return
+	// ignore health check and auth routes
+	const url = event.node.req.url || "";
+	if (
+		!url.startsWith("/api") ||
+		[
+			"/api/health",
+			"/api/auth/login",
+			"/api/auth/signup",
+			"/api/auth/refresh",
+		].includes(url)
+	)
+		return;
 
-  const authHeader = getHeader(event, 'authorization')
+	const authHeader = getHeader(event, "authorization");
 
-  if (!authHeader) {
-    throw createError({ statusCode: 401, message: 'No token provided' })
-  }
+	if (!authHeader) {
+		throw createError({ statusCode: 401, message: "No token provided" });
+	}
 
-  try {
-    const token = authHeader.split(' ')[1]
+	try {
+		const token = authHeader.split(" ")[1];
 
-    const userId = decodeToken(token)
-    event.context.userId = userId
-
-  } catch (error) {
-    throw createError({ statusCode: 401, message: 'Invalid token' })
-  }
-})
+		const userId = decodeToken(token);
+		event.context.userId = userId;
+	} catch (_error) {
+		throw createError({ statusCode: 401, message: "Invalid token" });
+	}
+});
