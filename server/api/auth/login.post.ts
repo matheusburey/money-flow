bd854b7a8909f4271a41eea328e59c745419e254
matheusburey <matheusbureidev@gmail.com>
2025-12-19 15:14:48 -0300

    chore: add variable in .env.example

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,39 +1,37 @@
-import { H3Event } from "h3";
-
 import { getUserByEmail } from "~~/server/db/users";
 import { userTransformer } from "~~/server/transformers/user";
 
-export default defineEventHandler(async (event: H3Event) => {
-	try {
-		const body = await readBody(event);
-		const { email, password } = body;
+export default defineEventHandler(async (event) => {
+  try {
+    const body = await readBody(event);
+    const { email, password } = body;
 
-		const user = await getUserByEmail(email);
+    const user = await getUserByEmail(email);
 
-		if (!user || !(await verifyPassword(password, user.password))) {
-			throw createError({
-				statusCode: 400,
-				message: "Email ou senha inválidos",
-			});
-		}
+    if (!user || !(await verifyPassword(password, user.password))) {
+      throw createError({
+        statusCode: 400,
+        message: "Email ou senha inválidos",
+      });
+    }
 
-		const accessToken = generateAccessToken(user.id);
-		const refreshToken = generateRefreshToken(user.id);
+    const accessToken = generateAccessToken(user.id);
+    const refreshToken = generateRefreshToken(user.id);
 
-		return {
-			accessToken,
-			refreshToken,
-			user: userTransformer(user),
-		};
-	} catch (error: any) {
-		console.error("Error logging in:", error);
-		if (error.statusCode) {
-			throw error;
-		}
-		throw createError({
-			statusCode: 500,
-			message:
-				"Ocorreu um erro ao processar seu registro. Por favor, tente novamente.",
-		});
-	}
+    return {
+      accessToken,
+      refreshToken,
+      user: userTransformer(user),
+    };
+  } catch (error: any) {
+    console.error("Error logging in:", error);
+    if (error.statusCode) {
+      throw error;
+    }
+    throw createError({
+      statusCode: 500,
+      message:
+        "Ocorreu um erro ao processar seu registro. Por favor, tente novamente.",
+    });
+  }
 });
--- server/db/categories.ts
+++ server/db/categories.ts
@@ -46,94 +46,94 @@ export const createDefaultCategories = async (
 	userId: string,
 ): Promise<void> => {
 	await prisma.category.createMany({
-		data: [
-			// ===== EXPENSE =====
-			{
-				name: "Saúde",
-				color: "#FF8A80", // vermelho suave (urgência / saúde)
-				icon: "HeartPlus",
-				userId,
-				type: "expense",
-			},
-			{
-				name: "Lazer",
-				color: "#FFCC80", // laranja suave (diversão)
-				icon: "Restaurant",
-				userId,
-				type: "expense",
-			},
-			{
-				name: "Casa",
-				color: "#A5D6A7", // verde suave (lar, natureza)
-				icon: "House",
-				userId,
-				type: "expense",
-			},
-			{
-				name: "Transporte",
-				color: "#81D4FA", // azul claro (mobilidade)
-				icon: "Bus",
-				userId,
-				type: "expense",
-			},
-			{
-				name: "Entretenimento",
-				color: "#CE93D8", // roxo suave (diversão)
-				icon: "Movie",
-				userId,
-				type: "expense",
-			},
-			{
-				name: "Educação",
-				color: "#FFF59D", // amarelo suave (conhecimento)
-				icon: "GraduationCap",
-				userId,
-				type: "expense",
-			},
-			{
-				name: "Presente",
-				color: "#F48FB1", // rosa suave (carinho)
-				icon: "Gift",
-				userId,
-				type: "expense",
-			},
+    data: [
+      // ===== EXPENSE =====
+      {
+        name: "Saúde",
+        color: "#FF8A80", // vermelho suave (urgência / saúde)
+        icon: "HeartPlus",
+        userId,
+        type: "expense",
+      },
+      {
+        name: "Lazer",
+        color: "#FFCC80", // laranja suave (diversão)
+        icon: "TentTree",
+        userId,
+        type: "expense",
+      },
+      {
+        name: "Casa",
+        color: "#A5D6A7", // verde suave (lar, natureza)
+        icon: "House",
+        userId,
+        type: "expense",
+      },
+      {
+        name: "Transporte",
+        color: "#81D4FA", // azul claro (mobilidade)
+        icon: "Car",
+        userId,
+        type: "expense",
+      },
+      {
+        name: "Entretenimento",
+        color: "#CE93D8", // roxo suave (diversão)
+        icon: "MonitorPlay",
+        userId,
+        type: "expense",
+      },
+      {
+        name: "Educação",
+        color: "#FFF59D", // amarelo suave (conhecimento)
+        icon: "GraduationCap",
+        userId,
+        type: "expense",
+      },
+      {
+        name: "Presente",
+        color: "#F48FB1", // rosa suave (carinho)
+        icon: "Gift",
+        userId,
+        type: "expense",
+      },
 
-			// ===== INCOME =====
-			{
-				name: "Salário",
-				color: "#81C784", // verde confiança
-				icon: "Banknote",
-				userId,
-				type: "income",
-			},
-			{
-				name: "Freelance",
-				color: "#4DB6AC", // verde-água (trabalho flexível)
-				icon: "HandCoins",
-				userId,
-				type: "income",
-			},
-			{
-				name: "Presente",
-				color: "#A5D6A7", // verde claro (entrada positiva)
-				icon: "Gift",
-				userId,
-				type: "income",
-			},
-			{
-				name: "Juros",
-				color: "#90CAF9", // azul claro (finanças / bancos)
-				icon: "Coins",
-				userId,
-				type: "income",
-			},
-			{
-				name: "Outros",
-				color: "#B39DDB", // roxo suave (categoria neutra)
-				icon: "PiggyBank",
-				userId,
-				type: "income",
-			},
-		],
-	});
+      // ===== INCOME =====
+      {
+        name: "Salário",
+        color: "#81C784", // verde confiança
+        icon: "Banknote",
+        userId,
+        type: "income",
+      },
+      {
+        name: "Freelance",
+        color: "#4DB6AC", // verde-água (trabalho flexível)
+        icon: "HandCoins",
+        userId,
+        type: "income",
+      },
+      {
+        name: "Presente",
+        color: "#A5D6A7", // verde claro (entrada positiva)
+        icon: "Gift",
+        userId,
+        type: "income",
+      },
+      {
+        name: "Juros",
+        color: "#90CAF9", // azul claro (finanças / bancos)
+        icon: "Coins",
+        userId,
+        type: "income",
+      },
+      {
+        name: "Outros",
+        color: "#B39DDB", // roxo suave (categoria neutra)
+        icon: "PiggyBank",
+        userId,
+        type: "income",
+      },
+    ],
+  });
 };
